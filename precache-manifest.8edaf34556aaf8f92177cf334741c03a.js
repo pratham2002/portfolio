4142e6bc16ee78d5ffcf1b9bb382579754c6ffb9
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-03-21 18:49:12 +0530

    Updates

---
+++ precache-manifest.8edaf34556aaf8f92177cf334741c03a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "458974331b633c342d9f",
+    "url": "/portfolio/static/js/main.45897433.chunk.js"
+  },
+  {
+    "revision": "1c7c241387391098d131",
+    "url": "/portfolio/static/js/2.1c7c2413.chunk.js"
+  },
+  {
+    "revision": "458974331b633c342d9f",
+    "url": "/portfolio/static/css/main.34b1a8b0.chunk.css"
+  },
+  {
+    "revision": "1c7c241387391098d131",
+    "url": "/portfolio/static/css/2.c1df4e48.chunk.css"
+  },
+  {
+    "revision": "cccd9296309171630aef4ad2f94adb3d",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c40b8379a3e44d33d5c04c9da209e10.js"
+  "/portfolio/precache-manifest.8edaf34556aaf8f92177cf334741c03a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
