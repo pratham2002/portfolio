e62007b0acf85ca06d814b38f43265aad58e9f92
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-03-21 18:15:02 +0530

    code made react-hook based

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Tabs, Tab, Grid, Cell } from 'react-mdl';
 import ReactProjects from './ReactProjects';
+import './style.css';
 
-class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { activeTab: 0 };
-    }
 
-    toggleCategories(params) {
-        if (this.state.activeTab === 0) {
-            return (<div><h1>Javascript</h1></div>)
-        } else if (this.state.activeTab === 1) {
-            return (<div><h1>NodeJS</h1></div>)
-        } else if (this.state.activeTab === 2) {
+function Projects() {
+    const [activeTab, setActiveTab] = useState(0);
+    
+    function toggleCategories() {
+        if (activeTab === 0) {
             return (
-                <ReactProjects />
+                <div className="projects-grid">
+                    <ReactProjects />
+                    <ReactProjects />
+                </div>
             )
-        } else if (this.state.activeTab === 3) {
-            return (<div><h1>AngularJs</h1></div>)
+        } else if (activeTab === 1) {
+            return (
+                <div className="projects-grid">
+                    <h1>Javascript</h1>
+                </div>
+            )
+        } else if (activeTab === 2) {
+            return (<div className="projects-grid"><h1>NodeJS</h1></div>)
+        } else if (activeTab === 3) {
+            return (<div className="projects-grid"><h1>AngularJs</h1></div>)
         } else {
             return (<div><h1>Projects not added yet</h1></div>)
         }
     }
 
-    render() {
-        return (
-            <div className="catagory-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>JavaScript</Tab>
-                    <Tab>NodeJS</Tab>
-                    <Tab>React</Tab>
-                    <Tab>AngularJs</Tab>
-                    <Tab>Java</Tab>
-                    <Tab>GraphQL</Tab>
-                </Tabs>
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
-                        <Cell col={12}>
-                            <div className="content">
-                                {this.toggleCategories()}
-                            </div>
-                        </Cell>
-                    </Grid>
-                </section>
-            </div>
-        )
-    }
+    return (
+        <div className="catagory-tabs">
+            <Tabs activeTab={activeTab} onChange={(tabId) => setActiveTab(tabId)} ripple>
+                <Tab>React</Tab>
+                <Tab>JavaScript</Tab>
+                <Tab>NodeJS</Tab>
+                <Tab>AngularJs</Tab>
+            </Tabs>
+            <Grid>
+                <Cell col={12}>
+                    <div className="content">
+                        {toggleCategories()}
+                    </div>
+                </Cell>
+            </Grid>
+        </div>
+    )
+
 }
 
 export default Projects;
