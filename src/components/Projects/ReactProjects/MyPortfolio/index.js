e533d49ef97f196e630f620bf95a4a416ebbc85a
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-03-21 14:12:25 +0530

    RFCard View Created and used

--- src/components/Projects/ReactProjects/MyPortfolio/index.js
+++ src/components/Projects/ReactProjects/MyPortfolio/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton } from 'react-mdl';
+import RFCard from '../../../../Views/RFCard'
+
+const card = {
+    style: { minWidth: '450', margin: 'auto' },
+    cardTitle: {
+        style: { color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXQWR1h2Dzvwrwd4Fmb39hAp1HrgDVz8SPGn0TBaQxgmiIE4jM) center/ cover' },
+        body: "My Portfolio Site"
+    },
+    cardText: {
+        "body": "A portfolio project created in react."
+    },
+    acctionButtons: ['Github'],
+    cardMenu: {
+        style: { color: '#fff' }
+    }
+};
 
 const MyPortfolioInReact = () => (
-    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXQWR1h2Dzvwrwd4Fmb39hAp1HrgDVz8SPGn0TBaQxgmiIE4jM) center/ cover' }}>
-                My Portfolio Site</CardTitle>
-            <CardText>
-                A portfolio project created in react.
-        </CardText>
-            <CardActions border>
-                <Button colored>GitHub</Button>
-            </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
-                <IconButton name="share" />
-            </CardMenu>
-        </Card>
+    <RFCard card={card}/>
 );
 
 export default MyPortfolioInReact;
