d9de0be35d511778794c1085ba55f6162eb35330
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-04-06 14:44:26 +0530

    resume basic stuff

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -32,7 +32,54 @@ const Resume = () => (
                 <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
             </Cell>
             <Cell col={8} className="resume-right-col">
-                Right Side
+                <div>
+                    <h2>Experience</h2>
+                    <Grid>
+                        <Cell col={3}>
+                            <h6>2017 - current</h6>
+                        </Cell>
+                        <Cell col={9}>
+                        <h6>Confluxsys Private Limited, Pune</h6>
+                            <p>A private orginization oriented towards building software products for security management, 
+                                with a small team of around 30+ members, helped me expand my horizon over professional and 
+                                trending technologies to build reliable solutions to adhere with customer needs.
+                            </p>
+                        </Cell>
+                    </Grid>
+                </div>
+                <div>
+                    <h2>Education</h2>
+                    <Grid>
+                        <Cell col={3}>
+                            <h6>2013 - 2017</h6>
+                        </Cell>
+                        <Cell col={9}>
+                            <h6>Pune Vidyarti Griha's College of Engineering and Technology, Pune</h6>
+                            <p>A place where I undergraduated from with my bachelorette degree in Computer Science Engineering,
+                                lots of cool and interesting stuff I learned here mostly theory,
+                                but a did provide me a excellent conceptional base to roar in IT Indusrty
+                            </p>
+                        </Cell>
+                        <Cell col={3}>
+                            <h6>2011 - 2013</h6>
+                        </Cell>
+                        <Cell col={9}>
+                            <h6>Vishwakerma Institue of Technology, Pune</h6>
+                            <p>Jr. College, where I was fort eh first time introducted to HTML and CSS, and ended up rooting
+                                me in web development. A life full of adventure and awesome friends that always do the most craziest things you could imagine,
+                                still alive!.
+                            </p>
+                        </Cell>
+                        <Cell col={3}>
+                            <h6>2000 - 2011</h6>
+                        </Cell>
+                        <Cell col={9}>
+                            <h6>Shri Sharda English Medium School, Ahmednagar</h6>
+                            <p>My School
+                            </p>
+                        </Cell>
+                    </Grid>
+                </div>
             </Cell>
         </Grid>
 
