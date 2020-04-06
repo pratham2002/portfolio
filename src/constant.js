9e3ac1b0f6991075a1122b48817027586412cc2e
Ritesh Firodiya <ritesh.firodiya@globant.com>
2020-04-06 12:51:19 +0530

    landing content moved to constant

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,21 @@
 export default {
-  dashboard: {},
+  dashboard: {
+    avatar:
+      "https://cdn1.iconfinder.com/data/icons/data-science-1-1/512/20-512.png",
+    banner: "Full Stack Developer",
+    skills: ["JavaScript", " HTML/CSS", " React", " NodeJS", " Express"],
+    socialAccounts: [
+      { icon: "fa fa-github-square", link: "https://github.com/riteshf" },
+      {
+        icon: "fa fa-linkedin-square",
+        link: "https://www.linkedin.com/in/riteshfirodiya/",
+      },
+      {
+        icon: "fa fa-facebook-square",
+        link: "https://www.facebook.com/ritesh.firodiya.58",
+      },
+    ],
+  },
   about: {},
   resume: {},
   contactMe: {},
--- src/views/Dashboard/landingpage.js
+++ src/views/Dashboard/landingpage.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import './style.css';
-import { Grid, Cell } from 'react-mdl';
+import React from "react";
+import "./style.css";
+import { Grid, Cell } from "react-mdl";
+
+// constants
+import constant from "../../constant";
 
 const LandingPage = () => (
-    <div style={{ width: '100%', margin: 'auto' }}>
-        <Grid className="landing-grid">
-            <Cell col={3}>
-                <img
-                    src="https://cdn1.iconfinder.com/data/icons/data-science-1-1/512/20-512.png"
-                    alt="img-avatar"
-                    className="img-avatar"
-                />
-            </Cell>
-            <Cell col={9}>
-                <div className="banner-text">
-                    <h1>Full Stack Web Developer</h1>
-                    <hr />
-                    <p>JavaScript | HTML/CSS | React | NodeJS | Express</p>
-                    <div className="social-links">
-                        {/* Github  */}
-                        <a href="https://github.com/riteshf" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-github-square" aria-hidden="true" />
-                        </a>
-                        {/* LinkedIn */}
-                        <a href="https://www.linkedin.com/in/riteshfirodiya/" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-linkedin-square" aria-hidden="true" />
-                        </a>
-                        {/* Facebook */}
-                        <a href="https://www.facebook.com/ritesh.firodiya.58" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-facebook-square" aria-hidden="true" />
-                        </a>
-                    </div>
-                </div>
-            </Cell>
-        </Grid>
-    </div>
+  <div style={{ width: "100%", margin: "auto" }}>
+    <Grid className="landing-grid">
+      <Cell col={3}>
+        <img
+          src={constant.dashboard.avatar}
+          alt="img-avatar"
+          className="img-avatar"
+        />
+      </Cell>
+      <Cell col={9}>
+        <div className="banner-text">
+          <h1>{constant.dashboard.banner}</h1>
+          <hr />
+          <p>{constant.dashboard.skills.toString()}</p>
+          <div className="social-links">
+            {constant.dashboard.socialAccounts.map(({ icon, link }, index) => (
+              <a
+                key={index}
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className={icon} aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </Cell>
+    </Grid>
+  </div>
 );
 
 export default LandingPage;
